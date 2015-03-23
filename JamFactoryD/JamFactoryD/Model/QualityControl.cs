c36f0243996f03a4dde5d6ff19011d737dfd3e02
Pvorret <phvorret@gmail.com>
2015-03-23 13:25:43 +0100

    Lidt facade og modeller

---
+++ JamFactoryD/JamFactoryD/Model/QualityControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamFactoryD.Model
+{
+    public class QualityControl
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Employee { get; set; }
+        public string Variant { get; set; }
+        public string TimeCheck { get; set; }
+        public List<QualityActivity> ActivityList { get; set; }
+
+
+
+        
+    }
+}
